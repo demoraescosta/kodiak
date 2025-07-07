6a2080c4e8d54b7e24d80b328227392bb74db789
André de Moraes Costa <an.moraescosta@gmail.com>
2025-07-07 16:49:40 -0300

    now using flanterm to draw text on screen

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,6 +3,11 @@
 #include <stdint.h>
 #include <limine.h>
 
+#include "types.h"
+
+#include "terminal/flanterm.h"
+#include "terminal/backends/fb.h"
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -79,6 +84,15 @@ int memcmp(const void *s1, const void *s2, size_t n) {
     return 0;
 }
 
+// TODO: this sucks!!
+size_t strlen(const char *str)
+{
+	const char *s;
+
+	for (s = str; *s; ++s)
+		;
+	return (s - str);
+}
 // halt catch and fire
 static void hcf(void) {
     for (;;) {
@@ -96,12 +110,30 @@ void kmain(void) {
         hcf();
     }
 
-    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+    struct limine_framebuffer* framebuffer = framebuffer_request.response->framebuffers[0];
 
-    for (size_t i = 0; i < 255; i++) {
-        volatile uint32_t *fb_ptr = framebuffer->address;
-        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
-    }
+    /* for (size_t i = 0; i < 255; i++) { */
+    /*     volatile uint32_t *fb_ptr = framebuffer->address; */
+    /*     fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff; */
+    /* } */
+
+    struct flanterm_context* ctx = flanterm_fb_init(
+            NULL, NULL, // TODO: implement malloc and free
+            framebuffer->address, framebuffer->width, framebuffer->height, framebuffer->pitch,
+            framebuffer->red_mask_size, framebuffer->red_mask_shift,
+            framebuffer->green_mask_size, framebuffer->green_mask_shift,
+            framebuffer->blue_mask_size, framebuffer->blue_mask_shift,
+            NULL, // no canvas
+            NULL, NULL, // default ansi colors
+            NULL, NULL, // "
+            NULL, NULL, // "
+            NULL, 0, 0, 0, // deafult font, font widht, height and spacing
+            0, 0, // scale_x, scale_y
+            8 // margin
+            );
+    
+    const char* msg = "Hello Kodiak!\n";
+    flanterm_write(ctx, msg, strlen(msg));
 
     hcf();
 }
